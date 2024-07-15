5c5b2e6720a124cc27634bc08cc59d54e87afa15
Charles Shea <chashea@microsoft.com>
2024-07-15 13:25:53 -0400

    fixed provider versions

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.5"
+  required_version = "~> 1.7"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.71"
+      version = "~> 3.105"
     }
     random = {
       source  = "hashicorp/random"
